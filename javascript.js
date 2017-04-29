3c7c0a80ddc2855560eb0533e7e4c86df0faa042
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-04-29 16:06:55 +0200

    Updated

--- javascript.js
+++ javascript.js
@@ -1,24 +1,50 @@
 $('.button-collapse').sideNav();
 
-$.get('https://amper.herokuapp.com/', function(e){
-  console.log(arguments);
+const get = (path, callback) => {
+  path = path.replace(/^\//, '').replace(/\/$/, '');
+  $.ajax({
+    url: `https://api.amperboard.com/${path}/`,
+    type: "GET",
+    crossDomain: true,
+    success: callback,
+    error: console.log.bind(console, 'Error:')
+  });
+};
+
+get('/items', data => {
+  const items = data.sort((a, b) => b.consumption - a.consumption).filter(a => a.consumption > 350);
+  const html = items.map(item => `
+    <div class="item item-${item.id} col s12 m2" style="height: ${item.consumption / 10}px; background: ${chance.color({format: 'rgb'}).replace(/\)/, ', 0.3)')};">
+      <img src="/public/washing.png">
+    </div>
+  `).join('');
+  $('.plan .estimation').html(html);
+});
+
+get('/past_day', data => {
+  console.log(data);
+});
+
+get('/reports/pending_tasks/', data => {
+  console.log(data);
 });
 
 
-$.ajax({
-  url: "https://amper.herokuapp.com/",
-  type: "GET",
-  crossDomain: true,
-  data: JSON.stringify({ a: 'b' }),
-  dataType: "json",
-  success: function (response) {
-      var resp = JSON.parse(response)
-      alert(resp.status);
-  },
-  error: function (xhr, status) {
-      alert("error");
-  }
+
+dragula($('.day').get());
+$('.drop').each((i, el) => {
+  dragula([...$('.estimation').get(), el], {
+    copy: function (el, source) {
+      return source && $(source).is('.estimation');
+    },
+    accepts: function (el, target) {
+      return target && !$(target).is('.estimation');
+    }
+  });
 });
+
+
+
 
 // Any of the following formats may be used
 var ctx = document.getElementById('history');
