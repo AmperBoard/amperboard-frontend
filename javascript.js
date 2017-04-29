b4c231d8be52ab8b1963ae0700b83d8158729317
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-04-30 01:53:44 +0200

    Updated

--- javascript.js
+++ javascript.js
@@ -11,6 +11,7 @@ const get = (path, callback) => {
   });
 };
 
+
 const post = (path, callback) => {
   path = path.replace(/^\//, '').replace(/\/$/, '');
   $.ajax({
@@ -22,74 +23,96 @@ const post = (path, callback) => {
   });
 };
 
+const color = i => {
+  return `rgb(${26 * i + 216 * (1 - i)}, ${35 * i + 27 * (1 - i)}, ${126 * i + 96 * (1 - i)})`;
+}
+
 get('/items', data => {
   const items = data.sort((a, b) => b.consumption - a.consumption).filter(a => a.consumption > 500);
   const html = items.map(item => `
-    <div class="item item-${item.id} draggable col s12 m2" style="height: ${item.consumption / 10}px; background: ${chance.color({format: 'rgb'}).replace(/\)/, ', 0.3)')};">
-      <img src="public/${item.name}.png">
+    <div class="item item-${item.id} draggable col s12 m2" style="background: ${color((item.consumption - 500) / 500)}; height: ${item.consumption / 10}px;">
+      <div class="consumption">${item.consumption} Wh</div>
+      <p>${item.name}</p>
     </div>
   `).join('');
   $('.plan .estimation').html(html);
 });
 
 get('/reports/past_day', data => {
-  data = data.map(one => {
-    if (typeof one.time === 'string') {
-      one.time = new Date(one.time);
-    }
-    return one;
-  });
-
-  const grouped = [];
-
-  for (let h = 0; h < 24; h++) {
-    grouped[h] = data.filter(item => item.time.getHours() === h);
-  }
-
-  const coor = grouped.reduce((obj, range, x) => {
-    obj[x] = {
-      x,
-      y: range.reduce((total, item) => {
-        return total + item.consumption;
-      }, 0)
-    };
-    return obj;
-  }, []);
-
-  // Any of the following formats may be used
-  var ctx = document.getElementById('history');
-  var scatterChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          datasets: [{
-              label: 'Hourly consumption',
-              data: coor,
-              backgroundColor: 'rgba(255, 99, 132, 0.2)',
-              borderColor: 'rgba(255,99,132,1)',
-          }]
-      },
-      options: {
-          scales: {
-              xAxes: [{
-                  type: 'linear',
-                  position: 'bottom'
-              }]
-          }
+  get('capacity-hour/past_day', capacity => {
+    console.log(data.map(one => one.time), capacity.map(one => one.hour));
+    data = data.map(one => {
+      if (typeof one.time === 'string') {
+        one.time = new Date(one.time);
       }
+      return one;
+    });
+
+    const grouped = [];
+
+    for (let h = 0; h < 24; h++) {
+      grouped[h] = data.filter(item => item.time.getHours() === h);
+    }
+
+    const coor = grouped.reduce((obj, range, x) => {
+      obj[x] = {
+        x,
+        y: range.reduce((total, item) => {
+          return total + item.consumption;
+        }, 0)
+      };
+      return obj;
+    }, []);
+
+    console.log(capacity.sort((a, b) => new Date(a.hour) - new Date(b.hour)).map(one => one.hour));
+    capacity = capacity.map(data => ({
+      x: new Date(data.hour).getHours(),
+      y: data.capacity
+    }));
+
+    // Any of the following formats may be used
+    var ctx = document.getElementById('history');
+    Chart.defaults.global.defaultFontColor = '#fff';
+    var scatterChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Hourly consumption',
+                data: coor,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+            }, {
+                label: 'Available capacity',
+                data: capacity,
+                backgroundColor: 'rgba(99, 255, 132, 0.2)',
+                borderColor: 'rgba(99,255,132,1)',
+            }]
+        },
+        options: {
+          scales: {
+            xAxes: [{
+              type: 'linear',
+              position: 'bottom'
+            }]
+          }
+        }
+    });
   });
 });
 
 get('/reports/pending_tasks', data => {
-  const html = data.map(task => `
-    <div class="col s12 m3">
+  const html = data.slice(0, 5).map(task => `
+    <div class="col s6 m12">
       <div class="card horizontal">
-        <div class="card-image">
-          <img src="/public/${task.item.name}.png">
-        </div>
-        <div class="card-stacked">
+          <!-- <img src="/public/${task.item.name}.png"> -->
           <div class="card-content">
-            <p>${task.item.name}</p>
-            <p>${new Date(task.date).getHours()}:${new Date(task.date).getMinutes()}</p>
+
+            <p>
+              <span class="time">
+                ${new Date(task.date).getHours()}:${new Date(task.date).getMinutes()}
+              </span>
+              ${task.item.name}
+            </p>
           </div>
         </div>
       </div>
