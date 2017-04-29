f9b4ce366cca3cf28fc4a0973ef7d495b9a60355
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-04-29 17:34:23 +0200

    Data

--- javascript.js
+++ javascript.js
@@ -15,21 +15,67 @@ get('/items', data => {
   const items = data.sort((a, b) => b.consumption - a.consumption).filter(a => a.consumption > 350);
   const html = items.map(item => `
     <div class="item item-${item.id} col s12 m2" style="height: ${item.consumption / 10}px; background: ${chance.color({format: 'rgb'}).replace(/\)/, ', 0.3)')};">
-      <img src="/public/washing.png">
+      <img src="public/washing.png">
     </div>
   `).join('');
   $('.plan .estimation').html(html);
 });
 
-get('/past_day', data => {
+get('/reports/past_day', data => {
   console.log(data);
+
+  // Any of the following formats may be used
+  var ctx = document.getElementById('history');
+  var scatterChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          datasets: [{
+              label: 'Scatter Dataset',
+              data: [{
+                  x: -10,
+                  y: 0
+              }, {
+                  x: 0,
+                  y: 10
+              }, {
+                  x: 10,
+                  y: 5
+              }],
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255,99,132,1)',
+          }]
+      },
+      options: {
+          scales: {
+              xAxes: [{
+                  type: 'linear',
+                  position: 'bottom'
+              }]
+          }
+      }
+  });
 });
 
-get('/reports/pending_tasks/', data => {
-  console.log(data);
+get('/reports/pending_tasks', data => {
+  const html = data.map(task => `
+    <div class="col s12 m4">
+      <div class="card horizontal">
+        <div class="card-image">
+          <img src="/public/washing.png">
+        </div>
+        <div class="card-stacked">
+          <div class="card-content">
+            <p>${task.item.name}</p>
+            <a class="waves-effect waves-light btn-flat">
+              <i class="material-icons left">schedule</i> ${task.date.split(':').slice(0, 2).join(':')}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `).join('');
+  $('.history .pending').html(html);
 });
-
-
 
 dragula($('.day').get());
 $('.drop').each((i, el) => {
@@ -41,46 +87,4 @@ $('.drop').each((i, el) => {
       return target && !$(target).is('.estimation');
     }
   });
-});
-
-
-
-
-// Any of the following formats may be used
-var ctx = document.getElementById('history');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
 });
